0a1e38bfb482a8bb3313e4495a79d85c5f556f39
qaw <osshun21@gmail.com>
2025-03-13 16:13:33 +0900

    fix: token 로직 수정

--- src/main/java/com/jle_official/token_service/member/adaptor/MemberAdapter.java
+++ src/main/java/com/jle_official/token_service/member/adaptor/MemberAdapter.java
@@ -3,32 +3,37 @@ package com.jle_official.token_service.member.adaptor;
 import com.jle_official.token_service.common.exception.MemberAPIException;
 import com.jle_official.token_service.member.dto.MemberInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @Component
 @RequiredArgsConstructor
 public class MemberAdapter {
-    private final static String MEMBER_API_URL = "http://localhost:8091/v1/members";
     private final RestTemplate restTemplate;
+
+    @Value("${jle.api.member}")
+    private String MEMBER_API_URL;
 
     public String getMemberCredential(String loginId) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> request = new HttpEntity<>(headers);
 
-        ResponseEntity<String> response = restTemplate.exchange(
+        ResponseEntity<Map> response = restTemplate.exchange(
                 MEMBER_API_URL+"/credential/"+loginId,
                 HttpMethod.GET,
                 request,
-                String.class
+                Map.class
         );
         if (response.getStatusCode() != HttpStatus.OK) {
             throw new MemberAPIException("getMemberCredential() error", response);
         }
 
-        return response.getBody();
+        return response.getBody().get("password").toString();
     }
 
     public MemberInfo getMemberInfo(String loginId) {
