636a8b3db6ce13adbf2ff1bf22b4a43f1e5318c8
qaw <osshun21@gmail.com>
2025-05-25 21:27:22 +0900

    fix: Adapter 에러 처리 추가, 쿠키 httponly 해제

--- src/main/java/com/jle_official/token_service/common/security/PrincipalDetailsService.java
+++ src/main/java/com/jle_official/token_service/common/security/PrincipalDetailsService.java
@@ -1,12 +1,12 @@
 package com.jle_official.token_service.common.security;
 
+import com.jle_official.token_service.common.exception.MemberAdapterException;
 import com.jle_official.token_service.member.adaptor.MemberAdapter;
 import com.jle_official.token_service.member.dto.MemberInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,11 +16,18 @@ public class PrincipalDetailsService implements UserDetailsService {
     private final MemberAdapter memberAdapter;
 
     @Override
-    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
-        MemberInfo memberInfo = memberAdapter.getMemberInfo(memberId);
-        String password = memberAdapter.getMemberCredential(memberId);
-        log.debug("member Id: {}", memberInfo.getMemberId());
-        log.debug("password: {}", password);
+    public UserDetails loadUserByUsername(String memberId) {
+        MemberInfo memberInfo = null;
+        String password = null;
+
+        try{
+            memberInfo = memberAdapter.getMemberInfo(memberId);
+            password = memberAdapter.getMemberCredential(memberId);
+            log.debug("member Id: {}", memberInfo.getMemberId());
+            log.debug("password: {}", password);
+        } catch (Exception e) {
+            throw new MemberAdapterException("loadUserByUsername error: "+e.getMessage());
+        }
 
         return new PrincipalDetails(memberInfo, password);
     }
